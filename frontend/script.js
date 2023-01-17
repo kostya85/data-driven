c176e0c4a77817ff7e6b2f4ec4600b2530b3f9f2
k.gorbunov <k.gorbunov@center-inform.ru>
2023-01-17 20:27:51 +0300

    Frontend added

---
+++ frontend/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+	$('h2').mousedown(function(){
+		let id = $(this).data('id');
+		$('.expand-container[data-id="' + id + '"]').fadeIn('slow');
+	});
+	
+	$('#classify-form').on('submit', function(event) {
+		event.preventDefault();
+		
+		$.ajax({
+			url: '/data-driven/classify/',
+			type: 'POST',
+			data: new FormData(this),
+			dataType: 'json',
+			success: function (data) {
+				console.log(data)
+				$("#notify").notify("Class: " + data.class + ', accuracy: ' + data.accuracy, 'success');
+			},
+			error: function (obj, error) {
+				console.error(error);
+			},
+			cache: false,
+			contentType: false,
+			processData: false
+		});
+	});
+});
